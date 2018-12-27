d726c20aed4cababf9d940e41c09d82032446990
atosatto <andrea.tosy@gmail.com>
2018-12-27 16:41:31 +0100

    Add some additional tests simulating incomplete Bird reponses

--- bird_socket_test.go
+++ bird_socket_test.go
@@ -27,6 +27,17 @@ func TestBirdShowProtocols(t *testing.T) {
 	assert.True("'show protocols' completed", completed, t)
 }
 
+func TestIncompleteBirdShowProtocols(t *testing.T) {
+	out := "show protocols\n" +
+		"2002-name     proto    table    state  since       info\n" +
+		"1002-device1  Device   master   up     2018-12-21 12:35:11\n" +
+		" direct1  Direct   master   up     2018-12-21 12:35:11\n" +
+		" kernel1  Kernel   master   up     2018-12-21 12:35:11\n"
+	completed := isBirdCommandCompleted([]byte(out))
+
+	assert.False("'show protocols' completed", completed, t)
+}
+
 func TestBirdShowStatus(t *testing.T) {
 	out := "show status\n" +
 		"1000-BIRD 1.6.4\n" +
@@ -38,4 +49,15 @@ func TestBirdShowStatus(t *testing.T) {
 	completed := isBirdCommandCompleted([]byte(out))
 
 	assert.True("'show protocols' completed", completed, t)
+}
+
+func TestIncompleteBirdShowStatus(t *testing.T) {
+	out := "show status\n" +
+		"1000-BIRD 1.6.4\n" +
+		"1011-Router ID is 192.168.1.9\n" +
+		" Current server time is 2018-12-27 12:15:01\n" +
+		" Last reboot on 2018-12-21 12:35:11\n"
+	completed := isBirdCommandCompleted([]byte(out))
+
+	assert.False("'show protocols' completed", completed, t)
 }
